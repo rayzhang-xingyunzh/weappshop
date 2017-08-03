b164ae0b08af45564f6907d1f73c512ba274cdea
Ray <zruiwh@cn.ibm.com>
2017-08-03 18:50:54 +0800

    details

--- pages/index/index.js
+++ pages/index/index.js
@@ -93,9 +93,9 @@ Page({
  */
   , data: {
     currentBannerIndex: 0,
-    bannerImages: ["http://i2.kiimg.com/599607/9662acabeca0acb6s.jpg",
+    bannerImages: ["http://mmsns.qpic.cn/mmsns/JWk88JUM5RciaZnyLyyQp2VDqgicE2O0nzakKQEgabqmicR0d4raxwPZw/0",
       "http://i2.kiimg.com/599607/01903977571d49aes.jpg"],
-    bannerOriginalImages: ["http://i2.kiimg.com/599607/9662acabeca0acb6.jpg",
+    bannerOriginalImages: ["http://mmsns.qpic.cn/mmsns/JWk88JUM5RciaZnyLyyQp2VDqgicE2O0nzakKQEgabqmicR0d4raxwPZw/0",
       "http://i2.kiimg.com/599607/01903977571d49ae.jpg"],
     rooms: [{
       origin: "http://i1.buimg.com/599607/a729729f18969f18.jpg",
@@ -133,6 +133,14 @@ Page({
       title: "3D山水全景套间",
       subTitle: "180元/天"
     }],
+    markups: [
+      "http://mmbiz.qpic.cn/mmbiz_jpg/JWk88JUM5RfyMMzCxrfcibveEoma3RDm9ha9PDvGC18oru9J0RP3wNq4PsNuB8AWQ0DlKHsJZiaOfP8mOxhYeFicQ/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1",
+      "http://mmbiz.qpic.cn/mmbiz_jpg/JWk88JUM5RfyMMzCxrfcibveEoma3RDm9ha9PDvGC18oru9J0RP3wNq4PsNuB8AWQ0DlKHsJZiaOfP8mOxhYeFicQ/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1",
+      "http://mmbiz.qpic.cn/mmbiz_jpg/JWk88JUM5RfyMMzCxrfcibveEoma3RDm9ha9PDvGC18oru9J0RP3wNq4PsNuB8AWQ0DlKHsJZiaOfP8mOxhYeFicQ/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1",
+      "http://mmbiz.qpic.cn/mmbiz_jpg/JWk88JUM5RfyMMzCxrfcibveEoma3RDm9ha9PDvGC18oru9J0RP3wNq4PsNuB8AWQ0DlKHsJZiaOfP8mOxhYeFicQ/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1",
+      "http://mmbiz.qpic.cn/mmbiz_jpg/JWk88JUM5RfyMMzCxrfcibveEoma3RDm9ha9PDvGC18oru9J0RP3wNq4PsNuB8AWQ0DlKHsJZiaOfP8mOxhYeFicQ/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1",
+      "http://mmbiz.qpic.cn/mmbiz_jpg/JWk88JUM5RfyMMzCxrfcibveEoma3RDm9ha9PDvGC18oru9J0RP3wNq4PsNuB8AWQ0DlKHsJZiaOfP8mOxhYeFicQ/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1"
+    ],
     sales: [{
       origin: "http://i1.buimg.com/599607/0b777f6781f4ad0f.jpg",
       image: "http://i1.buimg.com/599607/0b777f6781f4ad0ft.jpg",
@@ -150,7 +158,7 @@ Page({
       name: '狗狗大人宠物酒店(武汉钻石总店)'
     },
     tabSelectedIndex: 0,
-    introduction:"   此处应有文字若干行，暂请自行脑补",
+    introduction: "  狗狗大人宠物酒店是一家致力于打造全武汉宠物级高端住宿的宠物店，为主人外出导致的家中爱宠无法照顾提供方便以及免除后顾之忧。\n  狗狗大人宠物酒店所有宠物住宿房间均为独立单间。每个房间配备独立摄像头和一位贴心管家。您可以在任何地点24小时观看狗狗住店情况。私人管家独立负责您的爱宠，24小时待命。让您的爱宠远离笼养与孤独。\n  狗狗大人宠物酒店服务项目：酒店式宠物寄养。世界级NGCK宠物协会B级美容师坐镇美容。\n  宠物用品出售。",
     openTime: '9:30-22:00'
   }
 
